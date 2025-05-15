355b480a9c465bcd223c23717734531e3768937c
dariu <darius.studiosuipod@gmail.com>
2025-05-15 11:53:59 -0500

    finish video 8: error handling

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,17 @@
 const User = require("../models/user");
 
+const okStatusCode = require("../utils/errors");
+const createdStatusCode = require("../utils/errors");
+const badRequestStatusCode = require("../utils/errors");
+const notFoundStatusCode = require("../utils/errors");
+const serverErrorStatusCode = require("../utils/errors");
+
 const getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.status(200).send(users))
+    .then((users) => res.status(okStatusCode).send(users))
     .catch((err) => {
       console.error(err);
-      return res.status(500).send({ message: err.message });
+      return res.status(serverErrorStatusCode).send({ message: err.message });
     });
 };
 
@@ -13,13 +19,13 @@ const createUser = (req, res) => {
   const { name, avatar } = req.body;
 
   User.create({ name, avatar })
-    .then((user) => res.status(201).send(user))
+    .then((user) => res.status(createdStatusCode).send(user))
     .catch((err) => {
       console.error(err);
       if (err.name === "ValidationError") {
-        return res.status(400).send({ message: err.message });
+        return res.status(badRequestStatusCode).send({ message: err.message });
       }
-      return res.status(500).send({ message: err.message });
+      return res.status(serverErrorStatusCode).send({ message: err.message });
     });
 };
 
@@ -28,16 +34,16 @@ const getUserById = (req, res) => {
 
   User.findById(userId)
     .orFail()
-    .then((user) => res.status(200).send(user))
+    .then((user) => res.status(okStatusCode).send(user))
     .catch((err) => {
       console.error(err);
       if (err.name === "DocumentNotFoundError") {
-        return res.status(404).send({ message: err.message });
+        return res.status(notFoundStatusCode).send({ message: err.message });
       }
       if (err.name === "CastError") {
-        return res.status(400).send({ message: err.message });
+        return res.status(badRequestStatusCode).send({ message: err.message });
       }
-      return res.status(500).send({ message: err.message });
+      return res.status(serverErrorStatusCode).send({ message: err.message });
     });
 };
 
